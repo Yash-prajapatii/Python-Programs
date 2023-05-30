2b7e5ecb1bf6e4281cba1cd7bcb531be82a1ea0d
Yash prajapati <107560761+Yash-prajapatii@users.noreply.github.com>
2023-05-30 22:39:21 +0530

    Add files via upload

---
+++ Lists Exercise.py
@@ -0,0 +1,18 @@
+#___Lists Exercise___#
+numbers = []
+strings = []
+names = ["milu","achu","sanju","anku","chiku"]
+
+numbers.append(4)
+numbers.append(2)
+numbers.append(5)
+numbers.append(1)
+numbers.append(3)
+
+strings.append('Old friend circle but still strong')
+
+second_name = names[1]
+
+print(numbers)
+print(strings)
+print("The second name on the nameslist",second_name)
---
+++ Lists.py
@@ -0,0 +1,21 @@
+#Example 1:
+mylist=[]
+mylist.append(1)
+mylist.append(2)
+mylist.append(3)
+print(mylist[0])
+print(mylist[1])
+print(mylist[2])
+
+for x in mylist:
+    print(x)
+
+print("\n")
+
+#Example 2:
+mylist=[1,0,2,9,3,8,4,7,5,6]
+print(mylist[1])
+print(mylist[9])
+print(mylist[1])
+print(mylist[4])
+
