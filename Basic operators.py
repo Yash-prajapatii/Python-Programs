2676b8e7f97386f23bf4bd3c621130832ba2cd9f
Yash prajapati <107560761+Yash-prajapatii@users.noreply.github.com>
2023-06-01 14:33:00 +0530

    Add files via upload

---
+++ Basic operators.py
@@ -0,0 +1,55 @@
+#Basic operators:
+#addition
+A = 113 + 137
+print(A)
+
+#subtraction
+B = 267 - 133
+print(B)
+
+#multiplication
+C = 25  * 17
+print(C)
+
+#division
+D = 84 / 16
+print(D)
+
+#Combined all operators:
+number = 190 + 140 - 34 * 5 / 2
+print(number)
+
+#modulo
+E = 11914 % 5
+print(E)
+
+#Power
+F = 5 ** 2
+G = 4 ** 3
+print(F)
+print(G)
+
+#Using operators with strings:
+#adding strings
+file = "Python" + " " + "Cpp" + " " + "Java"
+print(file)
+
+#multiplying strings
+line = "Lotsoflove " * 5
+print(line)
+
+#Using operators with lists:
+#adding lists
+list1 = [1,2,3,4,5]
+list2 = [6,7,8,9,0]
+total = list1 + list2
+print(total)
+
+#multiplying lists
+A = [1,3,5]*4
+print(A)
+
+#___OR___/
+
+print([3,2,1]*2)
+
