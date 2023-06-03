1a7392d6ae732c79de66e3e70c85d457e15b0ddb
Yash prajapati <107560761+Yash-prajapatii@users.noreply.github.com>
2023-06-03 23:50:08 +0530

    Basic operators exercise.py

--- Basic operators exercise.py
+++ Basic operators exercise.py
@@ -13,3 +13,11 @@ if x_list.count(x) == 10 and y_list.count(y) == 10:
     print("Almost there...")
 if big_list.count(x) == 10 and big_list.count(y) == 10:
     print("Great!")
+
+#output
+'''
+x_list contains  10 objects
+y_list contains  10 objects
+big_list contains  20 objects
+Almost there...
+Great!
