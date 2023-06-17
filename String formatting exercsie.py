e2a28425086a279aa103da5ca6a31e4804bd6fc4
Yash prajapati <107560761+Yash-prajapatii@users.noreply.github.com>
2023-06-17 19:08:22 +0530

    Add files via upload

---
+++ String formatting exercsie.py
@@ -0,0 +1,3 @@
+data = ("John", "Doe", 53.44)
+format_string = "Hello %s %s. Your current balance is $%s."
+print(format_string % data)
---
+++ String formatting.py
@@ -0,0 +1,9 @@
+name = "sushi"
+print("Hello, %s!" % name)
+
+Name = "John"
+age = 22
+print("%s is %d years old." % (Name,age))
+
+mylist = [11,22,33,44,55]
+print("A List: %s" % mylist)
