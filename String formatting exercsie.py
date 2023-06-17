93c69d0e073779fa1ce524a8ab4308354e264129
Yash prajapati <107560761+Yash-prajapatii@users.noreply.github.com>
2023-06-17 19:11:57 +0530

    String formatting exercsie.py

--- String formatting exercsie.py
+++ String formatting exercsie.py
@@ -1,3 +1,8 @@
 data = ("John", "Doe", 53.44)
 format_string = "Hello %s %s. Your current balance is $%s."
 print(format_string % data)
+
+#output
+'''
+Hello John Doe. Your current balance is $53.44.
+'''
