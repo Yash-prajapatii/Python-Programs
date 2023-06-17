5d2cbab68dc22fc26a29d98bb51bc7a0c4e3ca70
Yash prajapati <107560761+Yash-prajapatii@users.noreply.github.com>
2023-06-17 19:10:51 +0530

    String formatting.py

--- String formatting.py
+++ String formatting.py
@@ -7,3 +7,10 @@ print("%s is %d years old." % (Name,age))
 
 mylist = [11,22,33,44,55]
 print("A List: %s" % mylist)
+
+#output
+'''
+Hello, sushi!
+John is 22 years old.
+A List: [11, 22, 33, 44, 55]
+'''
