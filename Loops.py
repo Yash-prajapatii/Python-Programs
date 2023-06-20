5ac9738266b96abda6bea82a3b162f2c6967f39a
Yash prajapati <107560761+Yash-prajapatii@users.noreply.github.com>
2023-06-21 00:00:48 +0530

    Loops.py

--- Loops.py
+++ Loops.py
@@ -5,15 +5,48 @@
 primes = [2,3,5,7]
 for prime in primes:
     print(prime)
+#output
+'''
+2
+3
+5
+7
+'''
 
 for A in range(5):
     print(A)
+#output
+'''
+0
+1
+2
+3
+4
+'''
     
 for x in range(1,9):
     print(x)
+#output
+'''
+1
+2
+3
+4
+5
+6
+7
+8
+'''
 
 for x in range(1,9,2):
     print(x)
+#output
+'''
+1
+3
+5
+7
+'''
 
 #while loop:
 
@@ -21,6 +54,14 @@ A = 0
 while A > 5:
     print(A)
     A+=1
+#output
+'''
+0
+1
+2
+3
+4
+'''
 
 #break and continue statements:
 
@@ -30,12 +71,34 @@ while True:
     b += 1
     if b >= 55:
         break
-
+#output
+'''
+50
+51
+52
+53
+54
+'''
 
 for x in range(25):
     if x % 2 == 0:
         continue
     print(x)
+#output
+'''
+1
+3
+5
+7
+9
+11
+13
+15
+17
+19
+21
+23
+'''
 
 #can we use "else" clause for loops?
 
@@ -45,7 +108,17 @@ while (X < 7):
     X += 1
 else:
     print("X value reached %d" %(X))
-
+#output
+'''
+0
+1
+2
+3
+4
+5
+6
+X value reached 7
+'''
 
 for i in range(1, 10):
     if(i % 5 == 0):
@@ -53,4 +126,10 @@ for i in range(1, 10):
     print(i)
 else:
     print("this is not printed because for loop is terminated because of break but not due to fail of condition")
-    
+#output
+'''
+1
+2
+3
+4
+'''
