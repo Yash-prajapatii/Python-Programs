fa52d38c02f94308b8d873af4a322bb8725ecf08
Yash prajapati <107560761+Yash-prajapatii@users.noreply.github.com>
2023-06-22 23:24:10 +0530

    Functions.py

--- Functions.py
+++ Functions.py
@@ -6,7 +6,7 @@ my_function()
 
 def my_function_with_args(username, greeting):
     print("Hello, %s , From My Function!, I wish you %s"%(username, greeting))
-my_function_with_args("Pre singh","a great year!")
+my_function_with_args("Tanu","a great year!")
 
 def sum_two_numbers(a, b):
         return a + b
@@ -16,6 +16,6 @@ print(x)
 #output
 '''
 Hello From My Function!
-Hello, Pre singh , From My Function!, I wish you a great year!
+Hello, Tanu , From My Function!, I wish you a great year!
 3
 ''
