d1bcbef2977a8491f0c87c12ff0d420bfa71bd9a
Yash prajapati <107560761+Yash-prajapatii@users.noreply.github.com>
2023-06-22 23:17:44 +0530

    Add files via upload

---
+++ Functions exercise.py
@@ -0,0 +1,14 @@
+def list_benefits():
+    return "More organized code", "More readable code", "Easier code reuse", "Allowing programmers to share and connect code together"
+
+
+def build_sentence(benefit):
+    return "%s is a benefit of functions!" % benefit
+
+
+def name_the_benefits_of_functions():
+    list_of_benefits = list_benefits()
+    for benefit in list_of_benefits:
+        print(build_sentence(benefit))
+
+name_the_benefits_of_functions()
---
+++ Functions.py
@@ -0,0 +1,14 @@
+##FUNCTIONS##
+
+def my_function():
+    print("Hello From My Function!")
+my_function()
+
+def my_function_with_args(username, greeting):
+    print("Hello, %s , From My Function!, I wish you %s"%(username, greeting))
+my_function_with_args("Pre singh","a great year!")
+
+def sum_two_numbers(a, b):
+        return a + b
+x = sum_two_numbers(1,2)
+print(x)
